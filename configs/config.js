8468ae604023b90ed3a2ebc86338a7bbe9a871d2
Camilo <cacevedo@lean-tech.io>
2019-12-17 22:57:20 -0500

    Added JWT params used on token creation process

--- configs/config.js
+++ configs/config.js
@@ -1,4 +1,4 @@
-request('dotenv').config();
+require('dotenv').config();
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
@@ -7,9 +7,11 @@ const config = {
     dbUsername: process.env.DB_USER,
     dbPassword: process.env.DB_PASSWORD,
     dbHost: process.env.DB_HOST,
-    dbName: process.env.DBN_NAME,
+    dbName: process.env.DB_NAME,
     dbDialect: process.env.DB_DIALECT,
     authJwtSecret: process.env.AUTH_JWT_SECRET,
+    expireTimeToken: process.env.EXPIRE_JWT_TOKEN_TIME,
+    saltCrypt: process.env.SALT_ROUNDS,
     apiVersion: process.env.API_VERSION
 };
 
