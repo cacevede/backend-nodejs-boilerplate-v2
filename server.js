87c75586440d42e541c54d0a9ff0900550f67039
Camilo <cacevedo@lean-tech.io>
2019-12-17 23:04:27 -0500

    Check winston middleware call. Was commented middleware to check SQL database connection, because is not already required. unhandleRejection proccess added

--- server.js
+++ server.js
@@ -5,17 +5,14 @@ const express = require('express');
 const morgan = require('morgan');
 
 /** REQUIRED PROYECT FILES */
-const { runServer } = require('./scripts/serverScript');
-const { winstonLogger } = require('./configs/winstonConfig');
 const { config } = require('./configs/config');
-const { 
-    logErrors,
-    wrapErrors,
-    errorHandler 
-} = require('./utils/middlewares/errorHandlers');
+const { winstonLogger } = require('./configs/winstonConfig');
+const { runServer } = require('./scripts/serverScript');
 const { notFoundHandler } = require('./utils/middlewares/notFoundHandler');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
+
     /** SQL Database test */
-const databaseConValidator = require('./utils/middlewares/databaseConValidator');
+//const databaseConValidator = require('./utils/middlewares/databaseConValidator');
 
 /** INITS */
 const server = express();
@@ -26,10 +23,10 @@ server.set('port', config.port || 3005);
 /** MIDDLEWARES */
     
     /** Morgan Instance and Winston Integration */
-server.use(morgan('combined'), { stream: winstonLogger().stream });
+server.use(morgan('combined', { stream: winstonLogger().stream }));
     
     /** Sequelize test database SQL connection */
-server.use(databaseConValidator.testSQLConnection());
+//server.use(databaseConValidator.testSQLConnection());
 
     /** Body parser set */
 server.use(express.json());
@@ -48,7 +45,7 @@ server.use(config.apiVersion, require('./routes/router'));
 /** START SERVER */
 runServer(server, server.get('port'));
 
-/** PROCCESS HANDLER ERRORS */
+/** PROCCESS HANDLER ERRORS */ 
 process.on('unhandledRejection', (error) => {
     console.log(error);
     process.exit(1);
