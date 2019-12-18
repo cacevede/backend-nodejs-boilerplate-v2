08fd7b8f34421ee17af89843c3a053ec0c348708
Camilo <cacevedo@lean-tech.io>
2019-12-17 22:59:35 -0500

    Added express package instance forgotten on past repository update

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,7 @@
-// Your router here
-const serverRoutes = require('express').Router();
+/** REQUIRED PROD DEPENDENCIES */
+const express = require('express');
+
+const serverRoutes = express();
 
 // Your routes call here
 serverRoutes.use('/users', require('../components/users/router'));
