26836f16286925608e93a46d15558a38e1d65c86
Camilo <cacevedo@lean-tech.io>
2019-12-17 22:58:24 -0500

    Modified message of server running. Now 20% more off template string

--- scripts/serverScript.js
+++ scripts/serverScript.js
@@ -2,7 +2,7 @@ const server = { };
 
 server.runServer = (expressInstance, serverPort) => {
     expressInstance.listen(serverPort, () => {
-        console.log('Server running on: http://localhost:', serverPort);
+        console.log(`Server running on: http://localhost:${serverPort}`);
     });
 };
 
