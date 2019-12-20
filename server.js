921343967e1611c78f3b2b37313cfafc8268e436
Camilo <cacevedo@lean-tech.io>
2019-12-20 10:00:06 -0500

    Added cors config function and implemented middleware CORS. Use it testSQLConnection on just for test on server file, pending for validate and make a good implementation

--- server.js
+++ server.js
@@ -13,24 +13,26 @@ const winstonLogger = require('./configs/winstonConfig');
 const runServer = require('./scripts/serverScript');
 const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 const { logErrors, wrapErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
-//const { setCorsConfig } = require('./utils/middlewares/corsConfig');
+const setCorsConfig = require('./utils/middlewares/corsConfig');
 
-    /** SQL Database test */
-//const databaseConValidator = require('./utils/middlewares/databaseConValidator');
+    /** SQL Database Connection - Test */
+const { getSequelizeSQLConnection } = require('./configs/databaseConnection');
+const { testSQLConnection } = require('./utils/middlewares/databaseConValidator');
 
 /** INITS */
 const server = express();
+const dbConnection = getSequelizeSQLConnection();
 
 /** MIDDLEWARES */
 
     /** CORS config */
-server.use(cors());
+server.use(cors(setCorsConfig()));
 
     /** Morgan Instance and Winston Integration */
 server.use(morgan('combined', { stream: winstonLogger.stream }));
     
     /** Sequelize test database SQL connection */
-//server.use(databaseConValidator.testSQLConnection());
+testSQLConnection(dbConnection);
 
     /** Body parser set */
 server.use(express.json());
